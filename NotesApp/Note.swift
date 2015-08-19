04217159f447d4aa8a3813824e6a84c8f762b104
Stefan Miller <stefanamiller@gmail.com>
2015-08-18 23:36:00 -0700

    Extract & cleanup NoteViewModel

--- NotesApp/Note.swift
+++ NotesApp/Note.swift
@@ -7,71 +7,8 @@
 //
 
 import UIKit
-import ReactiveCocoa
 
 class Note: NSObject {
    var dateUpdated: NSDate = NSDate()
    var body: String? = ""
-}
-
-class NoteViewModel: NSObject {
-   let note = MutableProperty<Note?>(nil)
-
-   let dateStamp = MutableProperty<String>("")
-   let noteBody  = MutableProperty<String?>("")
-   
-   override init() {
-      super.init()
-      
-      dateStamp <~ note.producer
-         |> ignoreNil
-         |> map { note in
-            note.dateUpdated
-         }
-//         |> on(next: { date in
-//            NSLog("Formatting date \(date)")
-//         })
-         |> self.formatUpdatedDate()
-
-      noteBody <~ note.producer
-         |> ignoreNil
-         |> map { note in
-            note.body
-         }
-         |> on(next: { value in
-            NSLog("Updated note body!")
-         })
-   }
-   
-   convenience init(_ n: Note?) {
-      self.init()
-      note.put(n)
-   }
-   
-   func formatUpdatedDate() -> Signal<NSDate, NoError> -> Signal<String, NoError> {
-      return map { date in
-         self.formatUpdatedDate(date)
-      }
-   }
-   
-   func formatUpdatedDate(date: NSDate) -> String {
-      if NSCalendar.currentCalendar().isDateInToday(date) {
-         return NSDateFormatter.localizedStringFromDate(date, dateStyle: .NoStyle, timeStyle: .ShortStyle)
-      }
-      else {
-         return NSDateFormatter.localizedStringFromDate(date, dateStyle: .ShortStyle, timeStyle: .ShortStyle)
-      }
-   }
-   
-   func updateTime() {
-      self.note.value?.dateUpdated = NSDate()
-   }
-   
-   func updateText(text: String?) {
-      self.note.value?.body = text
-      updateTime()
-//      let newNote = Note()
-//      newNote.body = text
-//      self.note.put(newNote)
-   }
 }
---
+++ NotesApp/NoteViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  NoteViewModel.swift
+//  NotesApp
+//
+//  Created by Stefan Miller on 2015-08-18.
+//  Copyright (c) 2015 Stefan Miller. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+
+class NoteViewModel: NSObject {
+   private let note = MutableProperty<Note?>(nil)
+   
+   let dateStamp = MutableProperty<String>("")
+   let noteBody  = MutableProperty<String?>("")
+   
+   init(_ n: Note?) {
+      super.init()
+      
+      setupBindings()
+      self.note.put(n)
+   }
+   
+   convenience override init() {
+      self.init(nil)
+   }
+   
+   func setupBindings() {
+      dateStamp <~ note.producer
+         |> ignoreNil
+         |> map { note in
+            note.dateUpdated
+         }
+         //         |> on(next: { date in
+         //            NSLog("Formatting date \(date)")
+         //         })
+         |> self.formatUpdatedDate()
+      
+      noteBody <~ note.producer
+         |> ignoreNil
+         |> map { note in
+            note.body
+         }
+         |> on(next: { value in
+            NSLog("Updated note body!")
+         })
+   }
+   
+   func formatUpdatedDate() -> Signal<NSDate, NoError> -> Signal<String, NoError> {
+      return map { date in
+         self.formatUpdatedDate(date)
+      }
+   }
+   
+   func formatUpdatedDate(date: NSDate) -> String {
+      if NSCalendar.currentCalendar().isDateInToday(date) {
+         return NSDateFormatter.localizedStringFromDate(date, dateStyle: .NoStyle, timeStyle: .MediumStyle)
+      }
+      else {
+         return NSDateFormatter.localizedStringFromDate(date, dateStyle: .ShortStyle, timeStyle: .MediumStyle)
+      }
+   }
+   
+   func updateText(text: String?) {
+      if let noteRaw = self.note.value {
+         noteRaw.body = text
+         noteRaw.dateUpdated = NSDate()
+         note.put(noteRaw)
+      }
+   }
+}
